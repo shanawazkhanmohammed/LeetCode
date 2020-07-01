1ba02ba5ada3a51e0c6e63bb1982fffeafdeea10
shanawazkhanmohammed <39298086+shanawazkhanmohammed@users.noreply.github.com>
2020-07-01 20:00:52 +0530

    Update coins.cpp

--- coins.cpp
+++ coins.cpp
@@ -1,9 +1,38 @@
+/* Question :-
+You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
+Given n, find the total number of full staircase rows that can be formed.
+n is a non-negative integer and fits within the range of a 32-bit signed integer.
+Example 1:
+n = 5
+The coins can form the following rows:
+¤
+¤ ¤
+¤ ¤
+Because the 3rd row is incomplete, we return 2.
+Example 2:
+n = 8
+The coins can form the following rows:
+¤
+¤ ¤
+¤ ¤ ¤
+¤ ¤
+Because the 4th row is incomplete, we return 3.*/
+
+/*Thought :-
+    Sum of coins for height H <= N
+            H*(H + 1)/2  <= N
+        H*H + H – 2*N <= 0
+Now by Quadratic formula 
+(ignoring negative root)
+
+Maximum H can be (-1 + √(1 + 8N)) / 2 */
+
+
+
+
+
 class Solution {
 public:
     int arrangeCoins(int n) {
-   return (sqrt(8*(long long )n+1)-1)/2;
-            
-                
-            
-    }
+   return (sqrt(8*(long long )n+1)-1)/2;}
 };  
